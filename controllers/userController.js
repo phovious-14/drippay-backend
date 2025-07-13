2f4a110e4fca2ffaafb79b00b4428f982d2573e5
Krishn Panchal <krishnpanchal14@gmail.com>
2025-07-13 10:25:18 +0530

    Refactor userController by removing unused influencer-related functions and clean up routes by removing unnecessary imports. This streamlines the user management functionality and improves code maintainability.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,44 +1,5 @@
-import Influencer from "../model/Influencer.js";
 import User from "../model/User.js";
 
-export const storeInfluencer = async (req, res) => {
-  try {
-    const { influencerId } = req.body;
-    const influencer = new Influencer({ influencerId });
-    await influencer.save();
-
-    res.status(200).json({ message: "Campaign created successfully" });
-  } catch (error) {
-    console.error("Error adding user:", error);
-    res.status(500).json({ error: "Failed to create campaign" });
-  }
-};
-
-export const getUsers = async (req, res) => {
-  try {
-    const users = await User.find();
-    res.status(200).json(users);
-  } catch (error) {
-    console.error("Error getting users:", error);
-    res.status(500).json({ error: "Failed to get users" });
-  }
-};
-
-export const getUserById = async (req, res) => {
-  try {
-    const { userId } = req.params;
-    const user = await User.findOne({ _id: userId });
-
-    if (!user) {
-      return res.status(404).json({ error: "User not found" });
-    }
-
-    res.status(200).json(user);
-  } catch (error) {
-    console.error("Error getting user:", error);
-    res.status(500).json({ error: "Failed to get user" });
-  }
-};
 
 export const getUserByPrivyId = async (req, res) => {
   try {
@@ -115,26 +76,5 @@ export const deleteUser = async (req, res) => {
   } catch (error) {
     console.error("Error deleting user:", error);
     res.status(500).json({ error: "Failed to delete user" });
-  }
-};
-
-export const updateUserRole = async (req, res) => {
-  try {
-    const { userPrivyId } = req.params; 
-    const { role } = req.body;
-    const user = await User.findOneAndUpdate(
-      { privyId: userPrivyId },
-      { $set: { role } },
-      { new: true }
-    );
-
-    if (!user) {
-      return res.status(404).json({ error: "User not found" });
-    }
-
-    res.status(200).json(user);
-  } catch (error) {
-    console.error("Error updating user role:", error);
-    res.status(500).json({ error: "Failed to update user role" });
   }
 };
