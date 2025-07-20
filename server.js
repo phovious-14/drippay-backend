176623641e1325614475e6b7767def9fb143a5ed
Krishn Panchal <krishnpanchal14@gmail.com>
2025-07-20 17:14:39 +0530

    Update CORS settings in server.js to allow all origins for improved accessibility.

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cors({ origin: true, credentials: true }));
 // Add headers before the routes are defined
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", ["https://www.drip-pay.xyz/", "http://localhost:3000"] );
+  res.setHeader("Access-Control-Allow-Origin", "*" );
 
   // Request methods you wish to allow
   res.setHeader(
